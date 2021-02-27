6ca2daf07bf6148f646e796fa553c31b6f2b789c
ZongchenLi <zoli@ucsd.edu>
2021-02-27 14:45:45 -0800

    update part1

--- Part1-Jest/__tests__/main.test.js
+++ Part1-Jest/__tests__/main.test.js
@@ -11,18 +11,18 @@ const volumnIcon = {
   
   describe('the volumn icon level', () => {
     test('is 1', () => {
-      expect(formatVolumeIconPath(volumnIcon.volumneVal1)).toBe(`./assets/media/icons/volume-level-1.svg`);
+      expect(formatVolumeIconPath(volumnIcon.volumneVal1)).toContain('volume-level-1.svg');
     });
   
     test('is 2', () => {
-        expect(formatVolumeIconPath(volumnIcon.volumneVal2)).toBe(`./assets/media/icons/volume-level-2.svg`);
+        expect(formatVolumeIconPath(volumnIcon.volumneVal2)).toContain('volume-level-2.svg');
     });
 
     test('is 3', () => {
-        expect(formatVolumeIconPath(volumnIcon.volumneVal3)).toBe(`./assets/media/icons/volume-level-3.svg`);
+        expect(formatVolumeIconPath(volumnIcon.volumneVal3)).toContain('volume-level-3.svg');
       });
 
     test('is 0', () => {
-        expect(formatVolumeIconPath(volumnIcon.volumneVal0)).toBe(`./assets/media/icons/volume-level-0.svg`);
+        expect(formatVolumeIconPath(volumnIcon.volumneVal0)).toContain('volume-level-0.svg');
     });
   });
