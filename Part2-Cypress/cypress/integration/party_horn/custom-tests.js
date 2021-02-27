a4b85994da43fbb2c1e14f0efa07e49ec3153d76
ZongchenLi <zoli@ucsd.edu>
2021-02-27 02:12:48 -0800

    update

---
+++ Part2-Cypress/cypress/integration/party_horn/custom-tests.js
@@ -0,0 +1,16 @@
+describe('Party Horn Tests', () => {
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:5500/');
+  });
+
+  it('First Test', () => {
+    expect(true).to.equal(true);
+  });
+
+  it('Slider changes when volume input changes', () => {
+
+    cy.get('#volume-number').clear().type('75')
+    expect(true).to.equal(true);
+  });
+
+});
